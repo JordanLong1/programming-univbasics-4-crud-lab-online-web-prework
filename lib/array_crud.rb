3e84c0dd9c90e01826fe457f44b5b1b5a7e51e2f
Jordan Long <jordanleelong5@gmail.com>
2019-09-19 01:59:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
 grocery_list = ["peanut butter", "jelly", "bread"]
-bread_list = grocerylist.pop
+grocery_list.pop("bread")
 end
 
 def remove_element_from_start_of_array(array)
