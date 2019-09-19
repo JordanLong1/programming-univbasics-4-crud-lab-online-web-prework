41dacaecea5378534e43911cea788642ce92aa2c
Jordan Long <jordanleelong5@gmail.com>
2019-09-19 01:36:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 ["Peanut Butter"]
-add_element_to_end_of_array.push("Jelly")
+add_element_to_end_of_array << ("Jelly")
 end
 
 def add_element_to_start_of_array(array, element)
