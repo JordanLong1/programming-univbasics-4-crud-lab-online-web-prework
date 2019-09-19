a0ad7fedab49346225a2f9f664479f85ede29449
Jordan Long <jordanleelong5@gmail.com>
2019-09-19 01:56:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
 kings_team = ["hield"]
-kings_team.unshift("fox")
+kings_team.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
